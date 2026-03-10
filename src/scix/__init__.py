7cb012ae4b119f03c39c9153fad97240eab3e444
Zoey Hu <54016472+zoeyzyhu@users.noreply.github.com>
2026-03-10 10:47:16 -0400

    Fix conflicting version management (#25)

--- src/scix/__init__.py
+++ src/scix/__init__.py
@@ -1,1 +1,8 @@
-from .__version__ import __version__ as __version__
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("scix")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
+
+__all__ = ["__version__"]
